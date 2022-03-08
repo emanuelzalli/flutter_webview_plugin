972f1ef6bbcef2fb392b681f8d7b676dcae50f21
Emanuel Zalli <emanuel@kodelabs.com>
2022-03-08 16:34:30 +0100

    AndroidX migration

--- example/android/app/src/main/java/com/yourcompany/flutter_webview_plugin_example/MainActivity.java
+++ example/android/app/src/main/java/com/yourcompany/flutter_webview_plugin_example/MainActivity.java
@@ -1,14 +1,7 @@
 package com.yourcompany.flutter_webview_plugin_example;
 
-import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
 
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(this);
-    }
 }
